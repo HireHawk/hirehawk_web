2a54158d213b1cb5d5f08c97d11788fea9d60b83
VolfP <pavel98volk@gmail.com>
2018-10-12 21:36:02 +0300

    Horrible CSS

--- hirehawk-react/src/components/BlockLink/index.jsx
+++ hirehawk-react/src/components/BlockLink/index.jsx
@@ -6,7 +6,7 @@ class BlockLink extends React.Component{
   render(){
     return(
       <a href='#'>
-        <div className='blockLink' style={{...this.props.style, backgroundImage:`url(${this.props.backgroundImage})`, backgroundSize:'cover'}}>
+        <div className={'blockLink '+this.props.className} style={{...this.props.style, backgroundImage:`url(${this.props.backgroundImage})`, backgroundSize:'cover'}}>
           <div className ='blockLink-text blockLink-text_black unselectable'>
               {this.props.name}
             </div>
--- hirehawk-react/src/pages/MainPage/index.jsx
+++ hirehawk-react/src/pages/MainPage/index.jsx
@@ -2,17 +2,18 @@ import React from 'react'
 
 //external dependencies
 import Button from '@material-ui/core/Button';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
 //internal dependencies
-import BlockLink from 'components/BlockLink'
-import OtherSimple from 'components/OtherSimple'
-import LoginButton from 'containers/LoginButton'
-import AdvertSearch from 'containers/AdvertSearch'
+import BlockLink from 'components/BlockLink';
+import HireHawkLogo from 'components/HireHawkLogo';
+import LoginButton from 'containers/LoginButton';
+import AdvertSearch from 'containers/AdvertSearch';
 // props and styles
 import './styles.css'
 // test data
 import Bike from 'test/media/images/bike.jpeg'
 import Hat from 'test/media/images/hat.jpeg'
+import HireHawkLogoImage from 'test/media/images/HireHawkLogoImage.png'
 
 class MainPage extends React.Component{
    constructor(props){
@@ -24,16 +25,17 @@ class MainPage extends React.Component{
    }
    handleLoginPopup(){
      alert('aa');
-     //<HireHawkLogo image={Hat}/>
    }
    render(){
     return (
     <div>
-
+      <HireHawkLogo image={HireHawkLogoImage} className = 'mainLogo'/>
       <LoginButton button={{style:{}, className:'loginButton'}}/>
       <AdvertSearch className ='advertSearch' > </AdvertSearch>
-      <BlockLink name = 'Give things!' onClick={this.handleGiveThingsClick} backgroundImage={Bike} style ={{position:'absolute',top:'40%',left:0,width:'50%',height:'40%',margin:'1px'}}/>
-      <BlockLink name = 'Take things!' onClick={this.handleGiveThingsClick} backgroundImage={Hat} style ={{position:'absolute',top:'40%',left:'50%',width:'50%',height:'40%',margin:'1px'}}/>
+      <BlockLink name = 'Give things!' className='giveThings' onClick={this.handleGiveThingsClick} backgroundImage={Bike} style ={{position:'absolute',top:'50%',right:'50%',width:'50%',height:'40%'}}/>
+      <BlockLink name = 'Take things!' className='takeThings' onClick={this.handleGiveThingsClick} backgroundImage={Hat} style ={{position:'absolute',top:'50%',left:'50%',width:'50%',height:'40%'}}/>
+      <div style = {{position:'absolute', top:'60%',left:0,width:'100%',background:'black',height:'40%',zIndex:-1}}/>
+      <div style = {{position:'absolute', bottom:0,left:0,width:'100%',textAlign:'center' , height:'8%', color:'white',fontSize:'200%'}}>This is a test design with horrible CSS. Do not judge.</div>
 
       {/*toChange*/}
       <Link to="/wellcome"><Button style={{float:'right'}}>Go To Wellcome page!</Button></Link>
