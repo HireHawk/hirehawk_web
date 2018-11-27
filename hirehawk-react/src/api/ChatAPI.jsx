e3d3a767c79950b91e7896394787e0974656ab8f
Maxim <maxim1998g@gmail.com>
2018-11-27 03:32:28 +0200

    Finish main functions for ChatAPI

--- hirehawk-react/src/api/ChatAPI.jsx
+++ hirehawk-react/src/api/ChatAPI.jsx
@@ -1,38 +1,18 @@
 import Config from 'config/api.json'
 import request from 'superagent'
 
-/*"chatapi":{
-    "uri":"",
-        "endPoints":{
-        "update":{
-            "getAllUnreadMessages": "updateChat/getAllUnreadMessages",
-                "getAllUserConversations": "updateChat/getAllUserConversations",
-                "getAllUserConversationMessages": "updateChat/getAllUserConversationMessages",
-        },
-        "admin": {
-            "addChat": "adminChat/addChat",
-                "setDialog": "adminChat/setDialog",
-                "addChatUser": "adminChat/addChatUser",
-                "removeChatUser": "adminChat/removeChatUser",
-                "deleteChat": "adminChat/deleteChat"
-        },
-        "chat": {
-            "addMessage": "addMessage",
-                "getChatById": "getChatById",
-                "getMessageById": "getMessageById",
-                "setMessagesAsReaded": "setMessagesAsReaded"
-        }
-    }
-}*/
-
 class ChatAPI {
     static createStartURL(fnPath) {
         return 'http://' + Config.chatapi.uri + '/' + fnPath;
     }
 
-    static getAllUnreadMessages(userId) {
+    /*          UPDATE          */
+
+    static getAllUnreadMessages(userId, token) {
       return  request
             .get(ChatAPI.createStartURL(Config.chatapi.endPoints.getAllUnreadMessages) + '/' + userId)
+            .set('Authorization', 'Bearer '+token)
+            .set('Content-Type', 'application/json')
             .on('error', err => {
                 alert('error');
             })
@@ -42,9 +22,9 @@ class ChatAPI {
             })
     };
 
-    static getAllUserConversations(userId,token) {
+    static getAllUserConversations(userId, token) {
         request
-            .get('http://' + Config.advertapi.uri + '/' + Config.advertapi.endPoints.getAllUserConversations)
+            .get(createStartURL(Config.chatapi.endPoints.getAllUserConversations) + '/' + userId)
             .set('Authorization', 'Bearer '+token)
             .set('Content-Type', 'application/json')
             .on('error', err => {
@@ -54,7 +34,146 @@ class ChatAPI {
                 alert('got JSON advert: ' + JSON.stringify(res.body));
             })
     };
-}
 
+    static getAllUserConversationMessages(userId, chatId, token) {
+        request
+            .get(createStartURL(Config.chatapi.endPoints.getAllUserConversationMessages) + '/' + userId + '/' + chatId)
+            .set('Authorization', 'Bearer '+token)
+            .set('Content-Type', 'application/json')
+            .on('error', err => {
+                alert('error');
+            })
+            .then((res) => {
+                alert('got JSON advert: ' + JSON.stringify(res.body));
+            })
+    };
+
+    /*          ADMIN           */
+
+    static addChat(name, token) {
+        request
+            .put(createStartURL(Config.chatapi.endPoints.addChat) + '/' + name + '/' + -1 + '/' + '')
+            .set('Authorization', 'Bearer '+token)
+            .set('Content-Type', 'application/json')
+            .on('error', err => {
+                alert('error');
+            })
+            .then((res) => {
+                alert('got JSON advert: ' + JSON.stringify(res.body));
+            })
+    };
+
+    static setDialog(firstId, secondId, token) {
+        request
+            .put(createStartURL(Config.chatapi.endPoints.setDialog) + '/' + firstId + '/' + secondId)
+            .set('Authorization', 'Bearer '+token)
+            .set('Content-Type', 'application/json')
+            .on('error', err => {
+                alert('error');
+            })
+            .then((res) => {
+                alert('got JSON advert: ' + JSON.stringify(res.body));
+            })
+    };
+
+    static addChatUser(chatId, userId, token) {
+        request
+            .put(createStartURL(Config.chatapi.endPoints.addChatUser) + '/' + chatId + '/' + userId)
+            .set('Authorization', 'Bearer '+token)
+            .set('Content-Type', 'application/json')
+            .on('error', err => {
+                alert('error');
+            })
+            .then((res) => {
+                alert('got JSON advert: ' + JSON.stringify(res.body));
+            })
+    };
+
+    static removeChatUser(chatId, userId, token) {
+        request
+            .put(createStartURL(Config.chatapi.endPoints.removeChatUser) + '/' + chatId + '/' + userId)
+            .set('Authorization', 'Bearer '+token)
+            .set('Content-Type', 'application/json')
+            .on('error', err => {
+                alert('error');
+            })
+            .then((res) => {
+                alert('got JSON advert: ' + JSON.stringify(res.body));
+            })
+    };
+
+    static deleteChat(chatId, token) {
+        request
+            .put(createStartURL(Config.chatapi.endPoints.deleteChat) + '/' + chatId)
+            .set('Authorization', 'Bearer '+token)
+            .set('Content-Type', 'application/json')
+            .on('error', err => {
+                alert('error');
+            })
+            .then((res) => {
+                alert('got JSON advert: ' + JSON.stringify(res.body));
+            })
+    };
+
+    /*          CHAT            */
+
+    // TODO: add and use Message struct
+    static addMessage(smth, token) {
+        return  request
+            .put(ChatAPI.createStartURL(Config.chatapi.endPoints.addMessage) + '/' + smth)
+            .set('Authorization', 'Bearer '+token)
+            .set('Content-Type', 'application/json')
+            .on('error', err => {
+                alert('error');
+            })
+            .then((res) => {
+                console.log('got JSON advert: ' + JSON.stringify(res.body));
+                return res.body;
+            })
+    };
+
+    static getChatById(chatId, token) {
+        return  request
+            .get(ChatAPI.createStartURL(Config.chatapi.endPoints.getChatById) + '/' + chatId)
+            .set('Authorization', 'Bearer '+token)
+            .set('Content-Type', 'application/json')
+            .on('error', err => {
+                alert('error');
+            })
+            .then((res) => {
+                console.log('got JSON advert: ' + JSON.stringify(res.body));
+                return res.body;
+            })
+    };
+
+    static getMessageById(id, token) {
+        return  request
+            .get(ChatAPI.createStartURL(Config.chatapi.endPoints.getMessageById) + '/' + id)
+            .set('Authorization', 'Bearer '+token)
+            .set('Content-Type', 'application/json')
+            .on('error', err => {
+                alert('error');
+            })
+            .then((res) => {
+                console.log('got JSON advert: ' + JSON.stringify(res.body));
+                return res.body;
+            })
+    };
+
+    static setMessagesAsReaded(receiverId, authorId, chatId, lastId, token) {
+        return  request
+            .put(ChatAPI.createStartURL(Config.chatapi.endPoints.setMessagesAsReaded)
+                + '/' + receiverId + '/' + authorId + '/' + chatId + '/' + lastId)
+            .set('Authorization', 'Bearer '+token)
+            .set('Content-Type', 'application/json')
+            .on('error', err => {
+                alert('error');
+            })
+            .then((res) => {
+                console.log('got JSON advert: ' + JSON.stringify(res.body));
+                return res.body;
+            })
+    };
+}
 
 export default ChatAPI;
